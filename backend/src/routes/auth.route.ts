01c45d817361e6f707c0e53205c696831f5efe16
Syed Bilal <syedbilal.dev27@gmail.com>
2026-09-25 04:10:54 -0700

    Add current user and refresh token routes

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import {
   changeCurrentPassword,
+  getCurrentUser,
+  refreshAccessToken,
   forgotPasswordRequest,
   loginUser,
   logout,
@@ -18,6 +20,10 @@ router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 
 router.route('/logout').post(verifyJWT, logout);
+
+router.route('/current-user').get(verifyJWT, getCurrentUser);
+
+router.route('/refresh-token').post(refreshAccessToken);
 
 router.route('/verify-email/:verificationToken').get(verifyEmail);
 
