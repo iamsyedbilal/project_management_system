a3ac3d8f50ae395c0e1375f2abc32fccb525888f
Syed Bilal <syedbilal.dev27@gmail.com>
2026-09-26 06:46:11 -0700

    feat: implement PRD authentication frontend

---
+++ frontend/src/api/project.api.ts
@@ -0,0 +1,4 @@
+import { api } from './axios'
+export const getProjects = async () => (await api.get('/projects')).data
+export const getProject = async (projectId: string) => (await api.get(`/projects/${projectId}`)).data
+export const createProject = async (data: { name: string; description?: string }) => (await api.post('/projects', data)).data
