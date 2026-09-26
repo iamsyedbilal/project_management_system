c7af37ba37cbe88bfe771981d2c3eef8a45216b6
Syed Bilal <syedbilal.dev27@gmail.com>
2026-09-26 06:23:48 -0700

    feat: add PRD frontend foundation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
-
+import { Navigate, Route, Routes } from 'react-router-dom'
+import './App.css'
 
 function App() {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<div className="page"><h1>Login</h1></div>} />
+      <Route path="/register" element={<div className="page"><h1>Register</h1></div>} />
+    </Routes>
   )
 }
 
